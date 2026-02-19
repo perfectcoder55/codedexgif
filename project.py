a8f150650428b35d2a4c31a85aed91e5355261dc
perfectcoder55 <jordanlee10011001@gmail.com>
2026-02-20 01:51:49 +0800

    Refactor image loading to use pathlib for filenames

--- project.py
+++ project.py
@@ -1,7 +1,4 @@
 import imageio.v3 as iio
-filenames = ['team-pic1.png', 'team-pic2.png']
-images = [ ]
-for filename in filenames:
-  images.append(iio.imread(filename))
-
-iio.imwrite('team.gif', images, duration = 500, loop = 0)
+from pathlib import Path
+images = [iio.imread(f) for f in Path('.').glob('team-pic*.png')]
+iio.imwrite('team.gif', images, duration=500, loop=0)
